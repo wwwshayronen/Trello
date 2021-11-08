f0cc380242dd61ab1d152c236b8f4823312f1d6f
Shay Ronen <47533456+wwwshayronen@users.noreply.github.com>
2021-11-08 17:27:46 +0200

    Update server.js

--- server.js
+++ server.js
@@ -30,6 +30,10 @@ app.use("/api/boards", boards);
 
 app.use(express.static(path.join(__dirname, 'client/build')));
 
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client/build", "index.html"));
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`server running on port ${PORT}`));
